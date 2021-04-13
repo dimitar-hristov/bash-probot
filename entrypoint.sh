4e18e0be46a101cfd5182c2b0273071740aa6ab4
dimitar-hristov <dimitarshr@gmail.com>
2021-04-13 22:05:03 +0100

    Change the dir

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,11 @@
 
 echo "Execute From Directory: $(pwd)"
 echo "Content: $(ls -al)"
-RESP=`npm install`
-echo "$RESP"
+CHANGE_DIR_RES=cd ${{ github.action_path }}
+echo "$CHANGE_DIR_RES"
+echo "Execute From Directory: $(pwd)"
+echo "Content: $(ls -al)"
+INSTALL_RESP=`npm install`
+echo "$INSTALL_RESP"
 RUN_RESP=`node index.js`
 echo "$RUN_RESP"
